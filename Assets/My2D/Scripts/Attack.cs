921ef8f4be65c1f2070a7dcf60e5d4c98cbf38e6
dksgytjr10 <dksgytjr10@gmail.com>
2024-10-16 17:29:25 +0900

    1.플레이어 공격, 데미지 , 죽음 상태 구현 2. 적 공격, 데미지 , 죽음 상태 구현 3. 스프라이트 페이드 아웃 효과 구현

--- Assets/My2D/Scripts/Attack.cs
+++ Assets/My2D/Scripts/Attack.cs
@@ -8,8 +8,25 @@ namespace My2D
     public class Attack : MonoBehaviour
     {
         //공격력
-
+        [SerializeField] private float attacKDamege = 10f;
         //충돌 체크해서 공격력 만큼 데미지 준다 
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            //데미지 입는 객체 찾기 
+            Damageable damageable = collision.GetComponent<Damageable>();
+
+            if (damageable != null)
+            {
+                damageable.TakeDamage(attacKDamege);
+                //Debug.Log($"{collision.name} {damageable.CurrentHealth}데미지를 입었다");
+               
+            }
+
+        }
+
+
     }
 
 }
+
+
--- Assets/My2D/Scripts/Damageable.cs
+++ Assets/My2D/Scripts/Damageable.cs
@@ -7,8 +7,108 @@ namespace My2D
 
     public class Damageable : MonoBehaviour
     {
-       //체력
+        #region Variables
+
+        private Animator animator;
+
+        //최대체력
+        [SerializeField] private float maxHealth = 100f;
+        public float MaxHealth
+        {
+            get
+            { return maxHealth; }
+            set
+            { maxHealth = value; }
+        }
+
+        //현재 체력
+        private float currentHealth;
+        public float CurrentHealth
+        {
+            get
+            {
+                return currentHealth;
+            }
+            private set
+            {
+                currentHealth = value;
+                //죽음 처리
+                if (currentHealth <= 0)
+                {
+                    //다이
+                    IsDeath = true;
+                    Debug.Log("죽음");
+                }
+            }
+        }
+
+        //죽음체크
+        private bool isDeath;
+        public bool IsDeath
+        {
+            get
+            { return isDeath; }
+            set
+            {
+                isDeath = value;
+                //애니메이션
+                animator.SetBool(AnimationString.IsDeath, value);
+
+            }
+        }
+
+        //무적모드
+        private bool isInvincible = false;
+      [SerializeField]  private float invincibleTimer = 2f;
+        private float countdown = 0f;
+
+        #endregion
+
+        void Awake()
+        {
+            //참조
+            animator = GetComponent<Animator>();
+
+         
+        }
+
+        private void Start()
+        {
+            //초기화
+            currentHealth = maxHealth;
+            countdown = invincibleTimer;
+            isDeath = false;
+        }
+
+        private void Update()
+        {
+            //무적상태이면 무적 타이머를 돌린다
+            if (isInvincible)
+            {
+                if (countdown <= 0f)
+                {
+                    isInvincible = false;
+                    //타이머 초기화
+                    countdown = invincibleTimer;
+                }
+                countdown -= Time.deltaTime;
+            }
+        }
 
         //TakeDamage
+        public void TakeDamage(float damage)
+        {
+            if (!IsDeath && !isInvincible)
+            {
+                //무적모드 초기화
+                isInvincible = true;
+
+
+                CurrentHealth -= damage;
+                Debug.Log($"{transform.name}의 현재 체력은{currentHealth}");
+                //애니메이션
+                animator.SetTrigger(AnimationString.HitTrigger);
+            }
+        }
     }
 }
